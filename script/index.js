86e3d2fa6559346114be5f84ebdfab1fbd11bf57
Michael8026 <55974274+Michael8026@users.noreply.github.com>
2023-03-16 09:01:16 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ const featureBlog = document.getElementById('feature-blog')
 const companyBlog = document.getElementById("company-blog");
 
 
-featureBtn.addEventListener('hover', () =>{
+featureBtn.addEventListener('click', () =>{
    if(featureBlog.classList.contains('display-none')){
     featureBlog.classList.remove('display-none')
    }
@@ -14,7 +14,7 @@ featureBtn.addEventListener('hover', () =>{
 })
 
 
-companyBtn.addEventListener("hover", () => {
+companyBtn.addEventListener("click", () => {
   if (companyBlog.classList.contains("display-none")) {
     companyBlog.classList.remove("display-none");
   } else {
